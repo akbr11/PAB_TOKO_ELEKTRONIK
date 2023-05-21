4e8af4dff0a6d01e9e1b5ffdb6718af2306c8b3c
akbr11 <ciremaiindah3078@gmail.com>
2023-05-21 20:10:12 +0700

    add DataHelper sqlite

---
+++ app/src/main/java/com/example/pab_toko_elektronik/DataHelper.java
@@ -0,0 +1,27 @@
+package com.example.pab_toko_elektronik;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "toko_elektronik.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table barang(namaBarang text null, kodeBarang text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db0, int db1, int db2) {
+
+    }
+}
