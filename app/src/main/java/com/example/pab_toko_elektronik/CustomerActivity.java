22c90977ba7242a201a10310e8e0f0b28df4d35b
akbr11 <ciremaiindah3078@gmail.com>
2023-05-23 20:11:35 +0700

    add CustomerActivity

--- app/src/main/java/com/example/pab_toko_elektronik/CustomerActivity.java
+++ app/src/main/java/com/example/pab_toko_elektronik/CustomerActivity.java
@@ -1,23 +1,93 @@
 package com.example.pab_toko_elektronik;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CustomerActivity extends AppCompatActivity {
-    LinearLayout allProduk;
-    TextView barang;
-    Button lihat;
+    String[] daftar;
+    ListView listView;
+    Menu menu;
+    protected Cursor cursor;
+    DataHelper dataHelper;
+    public static CustomerActivity ca;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
 
-        allProduk = findViewById(R.id.allProduct);
-        barang = findViewById(R.id.barang);
-        lihat = findViewById(R.id.lihat);
+        ca = this;
+        dataHelper = new DataHelper(this);
+        RefreshList();
+
+        ImageView btnlogout = findViewById(R.id.logout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Logout berhasil", Toast.LENGTH_SHORT).show();
+                Intent logout = new Intent(CustomerActivity.this, LoginActivity.class);
+                startActivity(logout);
+            }
+        });
+    }
+
+    public void RefreshList() {
+        SQLiteDatabase db = dataHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM barang", null);
+        daftar = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            cursor.moveToPosition(i);
+            daftar[i] = cursor.getString(0);
+        }
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
+        listView.setSelected(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView arg0, View arg1, int arg2, long arg3) {
+                final String selection = daftar[arg2];
+                final CharSequence[] dialogItem = {"Lihat Barang"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(CustomerActivity.this);
+                builder.setTitle("Pilihan");
+
+                builder.setItems(dialogItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int item) {
+                        switch (item) {
+                            case 0:
+                                Intent detail = new Intent(getApplicationContext(), DetailBarangActivity.class);
+                                detail.putExtra("namabarang", selection);
+                                startActivity(detail);
+                                break;
+                        }
+                    }
+                });
+                builder.create().show();
+            }
+        });
+        ((ArrayAdapter) listView.getAdapter()).notifyDataSetInvalidated();
     }
 }
---
+++ app/src/main/java/com/example/pab_toko_elektronik/DetailBarangActivity.java
@@ -0,0 +1,48 @@
+package com.example.pab_toko_elektronik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailBarangActivity extends AppCompatActivity {
+    protected Cursor cursor;
+    DataHelper dataHelper;
+    TextView namabarang, kodebarang, stockbarang, hargabarang;
+    ImageView btnback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_barang);
+        dataHelper = new DataHelper(this);
+        namabarang = findViewById(R.id.detailNamaBarang);
+        kodebarang = findViewById(R.id.detailKodeBarang);
+        stockbarang = findViewById(R.id.detailStokBarang);
+        hargabarang = findViewById(R.id.detailHargaBarang);
+        btnback = findViewById(R.id.back);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(DetailBarangActivity.this, CustomerActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+        SQLiteDatabase db = dataHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM barang WHERE namaBarang = '" +
+                getIntent().getStringExtra("namabarang") + "'", null);
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            cursor.moveToPosition(0);
+            namabarang.setText(cursor.getString(0));
+            kodebarang.setText(cursor.getString(1));
+            stockbarang.setText(cursor.getString(2));
+            hargabarang.setText(cursor.getString(3));
+        }
+    }
+}
